68e3285a0d7015711c361c12712e5f7b45a12d95
primetimeblues <davidsawyer41@gmail.com>
2017-02-05 13:54:58 -0700

    added material class

--- materials.py
+++ materials.py
@@ -1,1 +1,8 @@
-
+class Material:
+  'Represents a material definition'
+  
+  def __init__(self,name,epsilon,mu,conductivity):
+    self.name = name
+    self.epsilon = epsilon
+    self.mu = mu
+    self.conductivity = conductivity

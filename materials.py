8ee13b5c574593dd0ce902bac9687d004feb32f5
primetimeblues <davidsawyer41@gmail.com>
2017-02-08 00:12:35 -0700

    update materials

--- materials.py
+++ materials.py
@@ -6,3 +6,15 @@ class Material:
     self.epsilon = epsilon
     self.mu = mu
     self.conductivity = conductivity
+
+class MaterialLibrary:
+  'Collection of possible materials'
+  
+  def __init__(self):
+    self.library = {}
+    
+  def addmat(self, material):
+    self.library[material.name] = material
+    
+materials = MaterialLibrary()
+materials.addmat(Material('vacuum',1,1,0))

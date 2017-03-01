71244b4e79470f65ae59a27d2d1ab38ad24dd92c
primetimeblues <davidsawyer41@gmail.com>
2017-03-01 11:57:48 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,22 @@
+import geometry
 
+class Simulation:
+  'Collection of simulation parameters, geometric objects, etc.'
+  
+  def __init__(self, fLower,fUpper):
+    self.flower = fLower # lower frequency of the simulation
+    self.fupper = fUpper # upper frequency of the simulation
+    
+    c = 1 # I'll figure out units later
+    if fLower == 0:
+      wlMax = 4/fUpper
+    else:
+      wlMax = 1/fLower
+    qwl = wlMax/4 # quarter wavelength
+    xmin,ymin,zmin = -qwl,-qwl,-qwl
+    xmax,ymax,zmax = qwl,qwl,qwl
+    
+    self.domain = Domain(xmin,xmax,ymin,ymax,zmin,zmax)
+    self.objects = []
+    self.materials = []
+    self.mesh = None
